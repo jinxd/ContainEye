02523d2adb2d16705940bc6562c09279453c62e5
Jinxd <jinxd@users.noreply.github.com>
2025-04-16 11:33:08 +0200

    extemding bg time

--- ContainEye/App/AppDelegate.swift
+++ ContainEye/App/AppDelegate.swift
@@ -11,69 +11,108 @@ import Blackbird
 import StoreKit
 
 final class AppDelegate: NSObject, UIApplicationDelegate {
+    private var backgroundTaskID: UIBackgroundTaskIdentifier = .invalid
+    private var launched: Date? = nil
+
     func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
         var req = URLRequest(url: URL(string: "https://containeye.hannesnagel.com/push-notifications/update")!)
         req.httpMethod = "PUT"
         req.httpBody = deviceToken.map { String(format: "%02x", $0) }.joined().data(using: .utf8)
-        req.setValue("text/plain", forHTTPHeaderField: "Content-Type") // Set Content-Type
-        Task{ let _ = try? await URLSession.shared.data(for: req) }
+        req.setValue("text/plain", forHTTPHeaderField: "Content-Type")
+        Task {
+            let _ = try? await URLSession.shared.data(for: req)
+        }
     }
+
     func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: any Error) {
         Logger.telemetry("silent push notification registration failed", with: ["error" : error.generateDescription()])
     }
+
     func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
-        let testTask = Task{
+        print("started background task")
+        let backgroundTaskID = application.beginBackgroundTask(withName: "SilentPushBackgroundTask") {
+            print("completion handler no data")
+            completionHandler(.noData)
+        }
+
+        let testTask = Task {
             Logger.telemetry("received remote notification")
             let tests = (try? await ServerTest.read(from: SharedDatabase.db, matching: \.$credentialKey != "-", orderBy: .ascending(\.$lastRun), limit: 10)) ?? []
             let intent = TestServers()
-            intent.tests = tests.map{$0.entity}
+            intent.tests = tests.map { $0.entity }
+
             let _ = try? await intent.perform()
             let _ = try? await intent.donate()
+print("completion handler new data, ending bg task")
             completionHandler(.newData)
+            application.endBackgroundTask(backgroundTaskID)
         }
-        Task{
+
+        Task {
             try? await Task.sleep(for: .seconds(25))
-            guard !testTask.isCancelled else { return }
-            testTask.cancel()
+            if !testTask.isCancelled {
+                print("timeout, ending bg task")
+                testTask.cancel()
+                application.endBackgroundTask(backgroundTaskID)
+            }
         }
     }
 
-    private var launched = Date?.none
-
-    private func setLaunched(to date: Date?) {
-        launched = date
-    }
-    override init(){
+    override init() {
         Logger.initTelemetry()
 
         NotificationCenter.default.addObserver(forName: UIApplication.userDidTakeScreenshotNotification, object: nil, queue: .main) { _ in
             Logger.telemetry("screenshot.taken")
         }
+
         super.init()
+
         NotificationCenter.default.addObserver(forName: UIApplication.didBecomeActiveNotification, object: nil, queue: .current) { _ in
-            Task{await self.activated()}
+            Task { await self.activated() }
         }
+
         NotificationCenter.default.addObserver(forName: UIApplication.didEnterBackgroundNotification, object: nil, queue: .current) { _ in
-            Task{await self.hidden()}
+            Task { await self.hidden() }
         }
     }
-    func hidden() {
-        print("hidden")
-        if launched != nil {
-            Logger
-                .telemetry(
-                    "app.closed"
-                )
-            setLaunched(to: nil)
-        }
-    }
-    func activated() {
+
+    func activated() async {
         print("activated")
         if launched == nil {
-            setLaunched(to: .now)
+            launched = .now
             Logger.telemetry("app.launched")
         }
     }
+
+    func hidden() async {
+        print("hidden")
+        if launched != nil {
+            Logger.telemetry("app.closed")
+            launched = nil
+        }
+
+        let app = UIApplication.shared
+        backgroundTaskID = app.beginBackgroundTask(withName: "KeepAlive") {
+            app.endBackgroundTask(self.backgroundTaskID)
+            self.backgroundTaskID = .invalid
+        }
+
+        defer {
+            if backgroundTaskID != .invalid {
+                app.endBackgroundTask(backgroundTaskID)
+                backgroundTaskID = .invalid
+            }
+        }
+
+        Logger.telemetry("starting background task")
+        do {
+            try await Task.sleep(for: .seconds(29))
+            Logger.telemetry("background task done")
+        } catch {
+            Logger.telemetry("background task interrupted", with: ["error": error.localizedDescription])
+        }
+    }
+
     func fetchOriginalPurchaseDate() async -> Date? {
         for await result in Transaction.all {
             if case .verified(let transaction) = result {
